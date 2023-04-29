97aa0b1b397fa9724dec8cf74341f471eef5c2e6
Othman Djuliarso <37389043+odjuliarso@users.noreply.github.com>
2023-04-28 19:27:23 -0700

    added unit test to make sure the frequency of any word

--- Project39Tests/Project39Tests.swift
+++ Project39Tests/Project39Tests.swift
@@ -25,4 +25,11 @@ final class Project39Tests: XCTestCase {
         XCTAssertEqual(playData.allWords.count, 18440, "allWords was not 18440")
     }
     
+    // asserts wordCounts is giving back expected frequencies of a word
+    func testWordCountsAreCorrect() {
+        let playData = PlayData()
+        XCTAssertEqual(playData.wordCounts["TOUCHSTONE"], 84, "Home does not appear 84 times")
+        XCTAssertEqual(playData.wordCounts["constant"], 25, "Fun does not appear 25 times")
+        XCTAssertEqual(playData.wordCounts["just"], 45, "Mortal does not appear 45 times")
+    }
 }
