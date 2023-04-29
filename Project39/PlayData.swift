fe4da75f022b3dc6d03abbc674c545d54d8034ec
Othman Djuliarso <37389043+odjuliarso@users.noreply.github.com>
2023-04-28 19:17:21 -0700

    added dictionary to hold the frequency of each word that without duplicate words

--- Project39/PlayData.swift
+++ Project39/PlayData.swift
@@ -10,12 +10,18 @@ import Foundation
 
 class PlayData {
     var allWords = [String]()
-    
+    // a dictionary to check the frequency of any word
+    var wordCounts = [String: Int]()
+
     init() {
         if let path = Bundle.main.path(forResource: "plays", ofType: "txt") {
             if let plays = try? String(contentsOfFile: path) {
                 allWords = plays.components(separatedBy: CharacterSet.alphanumerics.inverted)
                 allWords = allWords.filter { $0 != "" }
+                for word in allWords {
+                    wordCounts[word, default:0] += 1
+                }
+                allWords = Array(wordCounts.keys)
 
             }
         }
